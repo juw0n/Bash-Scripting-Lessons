f730835deef8d48dbc55bf73e473b04fabf2ecb1
juw0n <oduns07@gmail.com>
2023-02-11 19:23:56 +0100

    update greeting file

--- greeting.sh
+++ greeting.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
-name="Juwon"
+name="Juwon!"
 echo "Good day $name"
-sleep 3
+sleep 2
 echo "You are looking great today, $name"
-sleep 3
+sleep 2
 echo "You have the best beard I've ever seen $name"
+sleep 4
+echo "what time is  it now?"
+read t
+echo "what have you get done today before $t"
+sleep 2
+echo "weldone! whatever you do, don't procastinate"
